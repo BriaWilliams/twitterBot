e670e7f26bc6fe243b49d169cf6ebfd2ebd94076
BriaWilliams <bria.williams128@gmail.com>
2020-12-15 14:46:12 -0500

    adding comments about keys

--- twiterBot/bot.py
+++ twiterBot/bot.py
@@ -9,14 +9,14 @@ from config import getApi
 
 
 api = getApi()
-nasaAPI = 'Rn9kSqjsLbjwABsvb2DFstOBjht6VRkesybxBSKh'
+nasaAPI = 'Rn8kSqjsLbjwTQsvb2DFstOBjht4PRkesybxBSKh'
 
 def tweetRoverPhotos():
     marsSol = 1000
     req = requests.get(
         f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={str(marsSol)}&camera=mast&api_key={nasaAPI}')
     dictionary = req.json()
-    index = 0
+    index = 4
 
     if index == len(dictionary['photos']) - 1:
         marsSol+=1
@@ -32,25 +32,8 @@ def tweetRoverPhotos():
             time.sleep(900)
             index +=1
 
-
 while True:
     tweetRoverPhotos()
-
-
-
-
-
-# def getRandomDic():
-#     '''Takes in dictionary with rovers as keys and cameras as values.
-#         If a specific rover key is selected then a random camera will be selected from its list of values.
-#          Returns chosen camera as a string'''
-#     if randomRovers == "curiosity":
-#         randomCam = random.choice(rovers['curiosity'])
-#     elif randomRovers == "opportunity":
-#         randomCam= random.choice(rovers['opportunity'])
-#     else:
-#         randomCam = random.choice(rovers['spirit'])
-
 
 
 
--- twiterBot/config.py
+++ twiterBot/config.py
@@ -1,6 +1,7 @@
 import twitter
 
 def getApi():
+    #All keys have been revoked
     return twitter.Api(consumer_key='yb33hvkIjNKu922ATZOj8LSEm',
                         consumer_secret='oFtlrhM723Yob8Spf4HOMRJyRFgMhdg1FU1yTnh5L8Hq1xpfLo',
                         access_token_key='1338233806172676098-V7FEIfTIz9w3NerLxS9LIVjTVqpKaW',
