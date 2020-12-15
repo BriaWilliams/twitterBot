d0da918227bcb8b9ee44d9ae03eb6ee31f1483e1
BriaWilliams <bria.williams128@gmail.com>
2020-12-15 15:59:35 -0500

    adding docstrings and comments

--- twiterBot/bot.py
+++ twiterBot/bot.py
@@ -12,11 +12,14 @@ api = getApi()
 nasaAPI = 'Rn8kSqjsLbjwTQsvb2DFstOBjht4PRkesybxBSKh'
 
 def tweetRoverPhotos():
-    marsSol = 1000
-    req = requests.get(
-        f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={str(marsSol)}&camera=mast&api_key={nasaAPI}')
+    """This function makes a request to the NASA API
+    by changing parameters in the URL. It uses a conditional that
+    loops through every photo taken on that Sol and post a tweet with the photo included."""
+
+    marsSol = 1000 #A Sol is a measurement for a day on Mars
+    req = requests.get(f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={str(marsSol)}&camera=mast&api_key={nasaAPI}')
     dictionary = req.json()
-    index = 4
+    index = 0
 
     if index == len(dictionary['photos']) - 1:
         marsSol+=1
@@ -29,7 +32,7 @@ def tweetRoverPhotos():
             status = f"MARTIAN DIRT ALERT: Check out this cool photo from the Curiosity Rover taken using the MAST Camera."
             api.PostUpdate(status, media="curiosity.jpg")
             os.remove("curiosity.jpg")
-            time.sleep(900)
+            time.sleep(900) #Post every 15 minutes
             index +=1
 
 while True:
